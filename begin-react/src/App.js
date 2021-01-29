a1372f6f63f835711b9a315fc4f4d994d591fb6d
sho221 <sho221@hanmail.net>
2021-01-29 22:51:15 +0900

    0129

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import Hello from './Hello';
+import Wrapper from './Wrapper';
+import Counter from './Counter';
+
+// function App() {
+//   const name ='react';
+//   const style = {
+//     backgroundColor : 'black',
+//     color:'aqua',
+//     fontSize : 24,
+//     padding : '1rem'
+//   }
+//   return (
+//     <div>
+//       <Hello />
+//       <div style={style}>{name}</div>
+//       <div className="gray-box"></div>
+//     </div>
+//   );
+
+  
+// }
+
+// function App() {
+//   return (
+//     <Wrapper>
+//     <Hello name="react" color="red"/>
+//     <Hello color="pink"/>
+//     </Wrapper>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <Wrapper>
+//       <Hello name="react" color="red" isSpecial={true}/>
+//       <Hello color="pink" />
+//     </Wrapper>
+//   )
+// }
 
 function App() {
   return (
-    <div>
-      <Hello />
-    </div>
+    <Counter />
   );
 }
+
+
 
 export default App;
