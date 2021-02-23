2413a3a5f203e05128e58068a15326b0fa493ed3
sho221 <sho221@hanmail.net>
2021-02-23 13:05:49 +0900

    0223

--- mashup-todolist/src/components/TodoList.js
+++ mashup-todolist/src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import TodoItem from './TodoItem';
+import { useTodoState } from '../TodoContext';
 
 const TodoListBlock = styled.div`
     flex:1;
@@ -11,12 +12,20 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
-    return <TodoListBlock>
-    <TodoItem text="프로젝트 생성하기" done={true} />
-    <TodoItem text="컴포넌트 스타일링 하기" done={true} />
-    <TodoItem text="Context 만들기" done={false} />
-    <TodoItem text="기능 구현하기" done={false} />
-  </TodoListBlock>;
+    const todos = useTodoState();
+  
+    return (
+      <TodoListBlock>
+        {todos.map(todo => (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            text={todo.text}
+            done={todo.done}
+          />
+        ))}
+      </TodoListBlock>
+    );
   }
   
   export default TodoList
